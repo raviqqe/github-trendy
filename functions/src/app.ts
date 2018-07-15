a7d3b3b9de380507245c40def5e7cf58ec43853d
Yota Toyama <raviqqe@gmail.com>
2018-07-15 15:11:30 -0700

    Enable CORS for functions

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,11 +1,13 @@
 import { graphqlExpress } from 'apollo-server-express';
 import * as bodyParser from 'body-parser';
+import cors = require('cors');
 import express = require('express');
 
 import schema from './schema';
 
 const app = express();
 
+app.use(cors({ origin: true }));
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
 export default app;
