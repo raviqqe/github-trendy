61cdefc6ff6d0365d8ff6029b50cdfce21f2bc76
Yota Toyama <raviqqe@gmail.com>
2018-07-18 13:58:05 -0700

    Initialize admin SDK properly

--- functions/src/firebase/index.ts
+++ functions/src/firebase/index.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
 import Languages from './languages';
 
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 
 export async function verifyToken(token: string): Promise<void> {
   await admin.auth().verifyIdToken(token);
