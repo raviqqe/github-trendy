e9d5540625c0da39284b784bec03a049d28618ff
Yota Toyama <raviqqe@gmail.com>
2018-07-24 00:17:37 -0700

    Configure Vue for custom meta tags

---
+++ app/vue.config.js
@@ -0,0 +1,15 @@
+const {
+	background_color,
+	short_name,
+	theme_color
+} = require("./public/manifest.json");
+
+module.exports = {
+	chainWebpack: config => {
+		config.plugin("pwa").tap(() => [{
+			themeColor: theme_color,
+			name: short_name,
+			msTileColor: background_color
+		}]);
+	}
+};
