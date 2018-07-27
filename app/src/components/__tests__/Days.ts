b39a72157cd863a4922ae23f2fa5d90536155b09
Yota Toyama <raviqqe@gmail.com>
2018-07-26 18:08:28 -0700

    Rename repositories component as days

--- app/src/components/__tests__/Days.ts
+++ app/src/components/__tests__/Days.ts
@@ -1,14 +1,14 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import Router from 'vue-router';
 
-import Repositories from '../Repositories.vue';
+import Days from '../Days.vue';
 
 const localVue = createLocalVue();
 
 localVue.use(Router);
 
 test('Render a component', () => {
-  shallowMount(Repositories, {
+  shallowMount(Days, {
     localVue,
     router: new Router()
   });
--- app/src/router.ts
+++ app/src/router.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Repositories from './components/Repositories.vue';
+import Days from './components/Days.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    { component: Repositories, path: '/' },
-    { component: Repositories, path: '/:language' }
+    { component: Days, path: '/' },
+    { component: Days, path: '/:language' }
   ],
   scrollBehavior() {
     return { x: 0, y: 0 };
