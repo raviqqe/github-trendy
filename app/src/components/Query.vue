908269506363fbdc31507eac246754e792ed8a6b
Yota Toyama <raviqqe@gmail.com>
2018-09-22 17:46:13 +0000

    Uninstall vue-loading-template
    It's even not compatible with SSR.

--- app/src/components/Query.vue
+++ app/src/components/Query.vue
@@ -5,28 +5,18 @@
     :variables="variables"
     fetchPolicy="cache-first"
   >
-    <template slot-scope="{ result: { data, loading } }">
-      <template v-if="loading || !data">
-        <VueLoading
-          type="spin"
-          color="darkgrey"
-          :style="{ margin: windowSmall ? 0 : 'auto' }"
-        />
-      </template>
-      <template v-else>
-        <slot v-bind="data" />
-      </template>
+    <template slot-scope="{ result: { data } }">
+      <slot v-bind="data" />
     </template>
   </ApolloQuery>
 </template>
 
 <script lang="ts">
 import * as vueApollo from "vue-apollo";
-import { VueLoading } from "vue-loading-template";
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component({
-  components: { ApolloQuery: (vueApollo as any).ApolloQuery, VueLoading }
+  components: { ApolloQuery: (vueApollo as any).ApolloQuery }
 })
 export default class extends Vue {
   @Prop(Object)
