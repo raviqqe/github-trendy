9f9d991e90a3cd812d8d51d5e61b9e593f099ce4
Yota Toyama <raviqqe@gmail.com>
2018-09-23 04:45:09 +0000

    Remove service worker module

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,10 +1,12 @@
+import { register } from "register-service-worker";
 import Vue from "vue";
 
 import App from "./components/App.vue";
 import createApolloProvider from "./infra/apollo";
-import "./register-service-worker";
 import createRouter from "./router";
 import createStore from "./store";
+
+register("/service-worker.js", {});
 
 Vue.config.productionTip = false;
 
