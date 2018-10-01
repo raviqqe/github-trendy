b190b76c6dffc748ba0d30ed28995c586dcfcaaa
Yota Toyama <raviqqe@gmail.com>
2018-10-01 08:45:02 -0700

    Record the first language view

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,6 +2,7 @@ import { register } from "register-service-worker";
 import Vue from "vue";
 
 import App from "./components/App.vue";
+import { pathToLanguageID } from "./domain";
 import createApolloProvider from "./infra/apollo";
 import createRouter from "./router";
 import createStore from "./store";
@@ -11,12 +12,15 @@ register("/service-worker.js", {});
 Vue.config.productionTip = false;
 
 (async () => {
+  const router = createRouter();
   const store = createStore();
 
   new Vue({
     apolloProvider: await createApolloProvider(store),
     render: h => h(App),
-    router: createRouter(),
+    router,
     store
   }).$mount("#app");
+
+  store.commit("viewLanguage", pathToLanguageID(router.currentRoute.path));
 })();
