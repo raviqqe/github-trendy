c0328934c33705768f779d2d7d6221ad41628a59
Yota Toyama <raviqqe@gmail.com>
2018-11-26 08:02:38 +0000

    Remove babel

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   collectCoverage: true,
   moduleFileExtensions: ["ts", "js", "json", "vue"],
+  preset: "ts-jest",
+  setupTestFrameworkScriptFile: "./src/set-up-tests.ts",
+  snapshotSerializers: ["jest-serializer-vue"],
+  testMatch: ["<rootDir>/src/**/__tests__/*.ts"],
   transform: {
     "^.+\\.vue$": "vue-jest",
     ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-      "jest-transform-stub",
-    "^.+\\.ts$": "ts-jest"
-  },
-  setupTestFrameworkScriptFile: "./src/set-up-tests.ts",
-  snapshotSerializers: ["jest-serializer-vue"],
-  testMatch: ["<rootDir>/src/**/__tests__/*.ts"]
+      "jest-transform-stub"
+  }
 };
